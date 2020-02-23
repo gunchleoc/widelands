1ad392ed17ac5f8c03fe03b24ea0f02e7084c181
GunChleoc <fios@foramnagaidhlig.net>
2020-02-23 14:34:54 +0000

    Spritesheet for Brownbear idle animation

--- data/world/critters/brownbear/init.lua
+++ data/world/critters/brownbear/init.lua
@@ -1,12 +1,6 @@
 dirname = path.dirname(__file__)
 
-animations = {
-   idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
-      hotspot = {21, 16},
-      fps = 20
-   },
-}
+animations = {}
 
 add_directional_animation(animations, "walk", dirname, "walk", {24, 24}, 4)
 
@@ -19,4 +13,15 @@ world:new_critter_type{
       remove = { "remove" },
    },
    animations = animations,
+   spritesheets = {
+      idle = {
+         directory = dirname,
+         basename = "idle",
+         fps = 20,
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         hotspot = { 20, 15 }
+      }
+   }
 }
