87636dbfb2b60a3bb16a245d5f439a3e76a53f43
GunChleoc <fios@foramnagaidhlig.net>
2020-03-01 18:44:45 +0000

    Spritesheets for lynx

--- data/world/critters/lynx/init.lua
+++ data/world/critters/lynx/init.lua
@@ -1,17 +1,5 @@
 dirname = path.dirname(__file__)
 
-animations = {
-   idle = {
-      directory = dirname,
-      basename = "idle",
-      hotspot = { 8, 14 },
-      fps = 20,
-   },
-}
-
-add_directional_animation(animations, "walk", dirname, "walk", {11, 21}, 25)
-
-
 world:new_critter_type{
    name = "lynx",
    descname = _ "Lynx",
@@ -20,5 +8,25 @@ world:new_critter_type{
    programs = {
       remove = { "remove" },
    },
-   animations = animations,
+   spritesheets = {
+      idle = {
+         directory = dirname,
+         basename = "idle",
+         fps = 20,
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         hotspot = { 8, 14 }
+      },
+      walk = {
+         directory = dirname,
+         basename = "walk",
+         fps = 25,
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         directional = true,
+         hotspot = { 11, 21 }
+      }
+   }
 }
