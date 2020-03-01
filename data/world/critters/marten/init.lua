8bb904a4cb530d2982bca000568cc1859360931d
GunChleoc <fios@foramnagaidhlig.net>
2020-03-01 09:48:08 +0000

    Raw export of marten + fix sliding

--- data/world/critters/marten/init.lua
+++ data/world/critters/marten/init.lua
@@ -2,13 +2,14 @@ dirname = path.dirname(__file__)
 
 animations = {
    idle = {
-      pictures = path.list_files(dirname .. "idle_??.png"),
+      directory = dirname,
+      basename ="idle",
       hotspot = { 11, 11 },
       fps = 20,
    },
 }
 
-add_directional_animation(animations, "walk", dirname, "walk", {15, 14}, 20)
+add_directional_animation(animations, "walk", dirname, "walk", {15, 14}, 50)
 
 world:new_critter_type{
    name = "marten",
