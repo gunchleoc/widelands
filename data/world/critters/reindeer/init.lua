5f8466d546b9c04bb15ac2962c1dc86aa1c58a04
GunChleoc <fios@foramnagaidhlig.net>
2020-03-01 19:10:29 +0000

    Spritesheets for reindeer

--- data/world/critters/reindeer/init.lua
+++ data/world/critters/reindeer/init.lua
@@ -1,17 +1,5 @@
 dirname = path.dirname(__file__)
 
-animations = {
-   idle = {
-      directory = dirname,
-      basename ="idle",
-      hotspot = { 23, 21 },
-      fps = 20,
-   },
-}
-
-add_directional_animation(animations, "walk", dirname, "walk", {25, 30}, 30)
-
-
 world:new_critter_type{
    name = "reindeer",
    descname = _ "Reindeer",
@@ -20,5 +8,25 @@ world:new_critter_type{
    programs = {
       remove = { "remove" },
    },
-   animations = animations,
+   spritesheets = {
+      idle = {
+         directory = dirname,
+         basename = "idle",
+         fps = 20,
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         hotspot = { 23, 18 }
+      },
+      walk = {
+         directory = dirname,
+         basename = "walk",
+         fps = 30,
+         frames = 20,
+         rows = 5,
+         columns = 4,
+         directional = true,
+         hotspot = { 25, 27 }
+      }
+   }
 }
